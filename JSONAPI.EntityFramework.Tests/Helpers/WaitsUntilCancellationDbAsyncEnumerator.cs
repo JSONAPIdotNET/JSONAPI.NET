3a2e993d75ab13b0eafe6cbb5fb702427ae32952
Chris Santero <chris@solveitsimply.com>
2015-01-23 14:22:23 -0500

    Create action filter that resolves an IQueryable asynchronously

---
+++ JSONAPI.EntityFramework.Tests/Helpers/WaitsUntilCancellationDbAsyncEnumerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JSONAPI.EntityFramework.Tests.Helpers
+{
+    internal class WaitsUntilCancellationDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
+    {
+        private readonly int _timeout;
+        private readonly IEnumerator<T> _inner;
+
+        public WaitsUntilCancellationDbAsyncEnumerator(int timeout, IEnumerator<T> inner)
+        {
+            _timeout = timeout;
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public async Task<bool> MoveNextAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(_timeout, cancellationToken);
+
+            return _inner.MoveNext();
+        }
+
+        public T Current
+        {
+            get { return _inner.Current; }
+        }
+
+        object IDbAsyncEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+}
