8ef16fa2d45f2e075b782156f1f5812430b24de7
Chris Santero <chris@solveitsimply.com>
2015-02-08 16:22:13 -0500

    simplify sample project
    Removed the todomvc tests project, and was able to simplify the example
    Startup code a bit.

--- JSONAPI.TodoMVC.API/Controllers/TodosController.cs
+++ JSONAPI.TodoMVC.API/Controllers/TodosController.cs
@@ -1,22 +1,9 @@
-﻿using JSONAPI.Core;
-using JSONAPI.EntityFramework;
-using JSONAPI.EntityFramework.Http;
+﻿using JSONAPI.EntityFramework.Http;
 using JSONAPI.TodoMVC.API.Models;
 
 namespace JSONAPI.TodoMVC.API.Controllers
 {
     public class TodosController : ApiController<Todo, TodoMvcContext>
     {
-        protected readonly TodoMvcContext DbContext;
-
-        public TodosController(TodoMvcContext dbContext)
-        {
-            DbContext = dbContext;
-        }
-
-        protected override IMaterializer MaterializerFactory()
-        {
-            return new EntityFrameworkMaterializer(DbContext);
-        }
     }
 }
