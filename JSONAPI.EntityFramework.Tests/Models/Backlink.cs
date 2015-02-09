4ea71f29ab630a435ffc735e92597033527d43ee
SphtKr <sphtkr@icloud.com>
2015-02-09 15:28:58 +0100

    Fixes a bug when determining a key not named `Id`

---
+++ JSONAPI.EntityFramework.Tests/Models/Backlink.cs
@@ -0,0 +1,18 @@
+﻿using JSONAPI.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSONAPI.EntityFramework.Tests.Models
+{
+    public class Backlink
+    {
+        [UseAsId]
+        [System.ComponentModel.DataAnnotations.Key]
+        public string Url { get; set; }
+
+        public Post Post { get; set; }
+        public string Snippet { get; set; }
+    }
+}
