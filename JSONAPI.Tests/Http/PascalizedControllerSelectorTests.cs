1d92961dc25bd428d4f1ba42244c203f5475ef76
Chris Santero <chris@solveitsimply.com>
2015-02-10 13:54:56 -0500

    test SelectController instead of GetControllerName

--- JSONAPI.Tests/Http/PascalizedControllerSelectorTests.cs
+++ JSONAPI.Tests/Http/PascalizedControllerSelectorTests.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
 using System.Web.Http.Routing;
 using FluentAssertions;
 using JSONAPI.Http;
@@ -13,37 +15,65 @@ namespace JSONAPI.Tests.Http
     [TestClass]
     public class PascalizedControllerSelectorTests
     {
-        [TestMethod]
-        public void Camelizes_controller_name_without_dashes()
+        private class FooBarBazQuxController
         {
-            TestForDefaultString("foo", "Foo");
+            
+        }
+
+        private class TestHttpControllerTypeResolver : IHttpControllerTypeResolver
+        {
+            public ICollection<Type> GetControllerTypes(IAssembliesResolver assembliesResolver)
+            {
+                return new List<Type>
+                {
+                    typeof (FooBarBazQuxController)
+                };
+            }
         }
 
         [TestMethod]
-        public void Camelizes_controller_name_with_underscores()
+        public void Selects_controller_for_all_lower_case_name()
         {
-            TestForDefaultString("foo_bar_baz", "FooBarBaz");
+            TestForDefaultString("foobarbazqux");
         }
 
         [TestMethod]
-        public void Camelizes_controller_name_with_dots()
+        public void Selects_controller_for_name_with_underscores()
         {
-            TestForDefaultString("foo.bar.baz", "FooBarBaz");
+            TestForDefaultString("foo_bar_baz_qux");
         }
 
         [TestMethod]
-        public void Camelizes_controller_name_with_dashes()
+        public void Selects_controller_for_name_with_dots()
         {
-            TestForDefaultString("foo-bar-baz", "FooBarBaz");
+            TestForDefaultString("foo.bar.baz.qux");
         }
 
         [TestMethod]
-        public void Camelizes_controller_name_with_all_three()
+        public void Selects_controller_for_name_with_dashes()
         {
-            TestForDefaultString("foo.bar-baz_qux", "FooBarBazQux");
+            TestForDefaultString("foo-bar-baz-qux");
         }
 
-        private void TestForDefaultString(string defaultString, string expectedTransformation)
+        [TestMethod]
+        public void Selects_controller_for_name_with_all_three()
+        {
+            TestForDefaultString("foo.bar-baz_qux");
+        }
+
+        [TestMethod]
+        public void Selects_controller_for_pascalized_name()
+        {
+            TestForDefaultString("FooBarBazQux");
+        }
+
+        [TestMethod]
+        public void Selects_controller_for_all_caps_name()
+        {
+            TestForDefaultString("FOOBARBAZQUX");
+        }
+
+        private void TestForDefaultString(string defaultString)
         {
             // Arrange
             var routeDataDict = new Dictionary<string, object>
@@ -52,23 +82,26 @@ namespace JSONAPI.Tests.Http
             };
 
             var mockRouteData = new Mock<IHttpRouteData>(MockBehavior.Strict);
+            mockRouteData.Setup(m => m.Route).Returns((IHttpRoute)null);
             mockRouteData.Setup(m => m.Values).Returns(routeDataDict);
 
+            var httpConfig = new HttpConfiguration();
+            httpConfig.Services.Replace(typeof(IHttpControllerTypeResolver), new TestHttpControllerTypeResolver());
+
             var mockRequestContext = new Mock<HttpRequestContext>(MockBehavior.Strict);
+            mockRequestContext.Setup(m => m.Configuration).Returns(httpConfig);
             mockRequestContext.Setup(m => m.RouteData).Returns(mockRouteData.Object);
 
             var request = new HttpRequestMessage();
             request.SetRequestContext(mockRequestContext.Object);
 
-            var mockHttpConfig = new Mock<HttpConfiguration>(MockBehavior.Strict);
-
-            var selector = new PascalizedControllerSelector(mockHttpConfig.Object);
+            var selector = new PascalizedControllerSelector(httpConfig);
 
             // Act
-            var actual = selector.GetControllerName(request);
+            var actual = selector.SelectController(request);
 
             // Assert
-            actual.Should().Be(expectedTransformation);
+            actual.ControllerType.Should().Be(typeof (FooBarBazQuxController));
         }
     }
 }
