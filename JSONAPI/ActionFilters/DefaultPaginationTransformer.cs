2bc4776706b689e5571ea984b577adf9af2aaa80
Chris Santero <chris@solveitsimply.com>
2015-03-13 14:48:26 -0400

    add pagination transformer

---
+++ JSONAPI/ActionFilters/DefaultPaginationTransformer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JSONAPI.ActionFilters
+{
+    /// <summary>
+    /// Performs pagination a
+    /// </summary>
+    public class DefaultPaginationTransformer : IQueryablePaginationTransformer
+    {
+        private const int DefaultPageSize = 100;
+
+        private readonly string _pageNumberQueryParam;
+        private readonly string _pageSizeQueryParam;
+        private readonly int? _maxPageSize;
+
+        /// <summary>
+        /// Creates a DefaultPaginationTransformer
+        /// </summary>
+        /// <param name="pageNumberQueryParam">The query parameter to use to indicate the page number</param>
+        /// <param name="pageSizeQueryParam">The query parameter to use to indicate the page size</param>
+        /// <param name="maxPageSize">The maximum page size to allow clients to request. Leave null for no restriction.</param>
+        public DefaultPaginationTransformer(string pageNumberQueryParam, string pageSizeQueryParam, int? maxPageSize = null)
+        {
+            if (maxPageSize <= 0) throw new ArgumentOutOfRangeException("maxPageSize", "The maximum page size must be 1 or greater.");
+
+            _pageNumberQueryParam = pageNumberQueryParam;
+            _pageSizeQueryParam = pageSizeQueryParam;
+            _maxPageSize = maxPageSize;
+        }
+
+        public IQueryable<T> ApplyPagination<T>(IQueryable<T> query, HttpRequestMessage request)
+        {
+            var hasPageNumberParam = false;
+            var hasPageSizeParam = false;
+            var pageNumber = 0;
+            var pageSize = _maxPageSize ?? DefaultPageSize;
+            foreach (var kvp in request.GetQueryNameValuePairs())
+            {
+                if (kvp.Key == _pageNumberQueryParam)
+                {
+                    hasPageNumberParam = true;
+                    if (!int.TryParse(kvp.Value, out pageNumber)) throw new HttpResponseException(HttpStatusCode.BadRequest);
+                }
+                else if (kvp.Key == _pageSizeQueryParam)
+                {
+                    hasPageSizeParam = true;
+                    if (!int.TryParse(kvp.Value, out pageSize)) throw new HttpResponseException(HttpStatusCode.BadRequest);
+                }
+            }
+
+            if (!hasPageNumberParam && !hasPageSizeParam)
+                return query;
+
+            if ((hasPageNumberParam && !hasPageSizeParam) || (!hasPageNumberParam && hasPageSizeParam))
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            if (pageNumber < 0 || pageSize < 0)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            if (_maxPageSize != null && pageSize > _maxPageSize.Value)
+                pageSize = _maxPageSize.Value;
+
+            var skip = pageNumber * pageSize;
+            return query.Skip(skip).Take(pageSize);
+        }
+    }
+}
---
+++ JSONAPI/ActionFilters/IQueryablePaginationTransformer.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Net.Http;
+
+namespace JSONAPI.ActionFilters
+{
+    /// <summary>
+    /// Provides a service to provide a page of data based on information from the request.
+    /// </summary>
+    public interface IQueryablePaginationTransformer
+    {
+        /// <summary>
+        /// Pages the query according to information from the request.
+        /// </summary>
+        /// <param name="query">The query to page</param>
+        /// <param name="request">The request message</param>
+        /// <typeparam name="T">The queryable element type</typeparam>
+        /// <returns>An IQueryable configured to return a page of data</returns>
+        IQueryable<T> ApplyPagination<T>(IQueryable<T> query, HttpRequestMessage request);
+    }
+}
