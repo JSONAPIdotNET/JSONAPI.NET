d1256afc05d064f035e4688f93d7af0c9c579d74
Chris Santero <chris@solveitsimply.com>
2015-07-08 23:36:02 -0400

    simplify async queryable enumeration transformer
    There was no need to be doing all that reflection.

--- JSONAPI.EntityFramework/ActionFilters/AsynchronousEnumerationTransformer.cs
+++ JSONAPI.EntityFramework/ActionFilters/AsynchronousEnumerationTransformer.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using JSONAPI.ActionFilters;
 using JSONAPI.QueryableTransformers;
 
 namespace JSONAPI.EntityFramework.ActionFilters
@@ -14,18 +11,9 @@ namespace JSONAPI.EntityFramework.ActionFilters
     /// </summary>
     public class AsynchronousEnumerationTransformer : IQueryableEnumerationTransformer
     {
-        private readonly Lazy<MethodInfo> _toArrayAsyncMethod = new Lazy<MethodInfo>(() =>
-               typeof(QueryableExtensions).GetMethods().FirstOrDefault(x => x.Name == "ToArrayAsync" && x.GetParameters().Count() == 2));
-
         public async Task<T[]> Enumerate<T>(IQueryable<T> query, CancellationToken cancellationToken)
         {
-            var queryableElementType = typeof (T);
-            var openToArrayAsyncMethod = _toArrayAsyncMethod.Value;
-            var toArrayAsyncMethod = openToArrayAsyncMethod.MakeGenericMethod(queryableElementType);
-            var invocation = (dynamic)toArrayAsyncMethod.Invoke(null, new object[] { query, cancellationToken });
-
-            var resultArray = await invocation;
-            return resultArray;
+            return await query.ToArrayAsync(cancellationToken);
         }
     }
 }
