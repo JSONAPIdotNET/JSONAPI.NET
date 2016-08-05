5ae40e89f2be28b408a23be808a3dd9e237d2066
Simon Hofer <simon.hofer@sbnet.ch>
2016-08-05 16:48:32 +0200

    Feat: context path for api (#114)
    * extended JsonApiHttpConfiguration to allow BaseUrlService with configured context path
    
    * Feat: context path for api
    - option to configure context path for api which will be respected in routing and for urls in response
    - option to configure the public origin address which will be used in response urls
    closes #112

--- JSONAPI.AcceptanceTests.EntityFrameworkTestWebApp/Properties/AssemblyInfo.cs
+++ JSONAPI.AcceptanceTests.EntityFrameworkTestWebApp/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -32,3 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("JSONAPI.AcceptanceTests.EntityFrameworkTestWebApp.Tests")]
